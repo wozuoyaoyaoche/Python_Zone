db09a45d45a1cbe01182a8b564315de2553a7f7b
wzyyc_windows <wozuoyaoyaoche@outlook.com>
2023-04-06 12:33:42 +0800

    windows

---
+++ NORMAL_CODE/enumerate枚举.py
@@ -0,0 +1,6 @@
+lst = ['apple', 'banana', 'orange', 'pear']
+for i, item in enumerate(lst):
+    print(f"Looping at index {i} for item {item}")
+
+#在上面的代码中，enumerate()将列表lst转换为一系列(index, item)元组，其中index是元素的索引，item是元素本身。然后，在for循环中，我们分别将这些元素赋值给i和item，然后打印输出当前循环的索引和元素。
+
